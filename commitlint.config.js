7f5a50e9a1a6c25c14be6a697d5b95ebdbfc25a2
MetaDow <9354693215@qq.com>
2023-04-26 14:42:10 +0800

    feat(init): init

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'], // extends can be nested
-  //parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format'
 }
--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['pnpm eslint --cache --fix', 'pnpm prettier  --write'],
+  '*.{js,jsx,ts,tsx}': ['pnpm eslint --cache --fix', 'pnpm prettier --cache --write'],
   '*.vue': ['pnpm eslint --cache --fix', 'pnpm stylelint --fix', 'pnpm prettier --write'],
   '*.{scss,less,style,html}': ['pnpm stylelint --fix', 'pnpm prettier --write']
 }
